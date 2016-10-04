768cd9a82503764222060005e1f32e22b39d11c1
Facundo Wasinger <wasingerf.acundo@gmail.com>
2016-10-03 23:48:55 -0300

    init
    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace pingServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtIPServer = new System.Windows.Forms.TextBox();
+            this.txtPortConnect = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtServerPort = new System.Windows.Forms.TextBox();
+            this.cmbIP = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbIP);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtServerPort);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(359, 67);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Server";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(233, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.txtPortConnect);
+            this.groupBox2.Controls.Add(this.txtIPServer);
+            this.groupBox2.Location = new System.Drawing.Point(13, 85);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(358, 70);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Cliente";
+            // 
+            // txtIPServer
+            // 
+            this.txtIPServer.Location = new System.Drawing.Point(6, 39);
+            this.txtIPServer.Name = "txtIPServer";
+            this.txtIPServer.Size = new System.Drawing.Size(159, 20);
+            this.txtIPServer.TabIndex = 0;
+            // 
+            // txtPortConnect
+            // 
+            this.txtPortConnect.Location = new System.Drawing.Point(171, 39);
+            this.txtPortConnect.Name = "txtPortConnect";
+            this.txtPortConnect.Size = new System.Drawing.Size(56, 20);
+            this.txtPortConnect.TabIndex = 1;
+            this.txtPortConnect.Text = "7300";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(233, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Ping Server";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "IP Server";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(168, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Port";
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.AcceptsReturn = true;
+            this.txtStatus.AcceptsTab = true;
+            this.txtStatus.Location = new System.Drawing.Point(13, 161);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtStatus.Size = new System.Drawing.Size(358, 208);
+            this.txtStatus.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(168, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Port";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "IP Server";
+            // 
+            // txtServerPort
+            // 
+            this.txtServerPort.Location = new System.Drawing.Point(171, 41);
+            this.txtServerPort.Name = "txtServerPort";
+            this.txtServerPort.Size = new System.Drawing.Size(56, 20);
+            this.txtServerPort.TabIndex = 6;
+            this.txtServerPort.Text = "7300";
+            // 
+            // cmbIP
+            // 
+            this.cmbIP.FormattingEnabled = true;
+            this.cmbIP.Location = new System.Drawing.Point(7, 40);
+            this.cmbIP.Name = "cmbIP";
+            this.cmbIP.Size = new System.Drawing.Size(158, 21);
+            this.cmbIP.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 381);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Ping Server";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtPortConnect;
+        private System.Windows.Forms.TextBox txtIPServer;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtServerPort;
+        private System.Windows.Forms.ComboBox cmbIP;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+using System.Threading;
+
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace pingServer
+{
+    public partial class Form1 : Form
+    {
+
+        public static string data = null;
+
+        void StartServer()
+        {
+            this.txtStatus.Text += "Server init... \r\n";
+
+            // Data buffer for incoming data.
+            byte[] bytes = new Byte[1024];
+
+            // Establish the local endpoint for the socket.
+            // Dns.GetHostName returns the name of the 
+            // host running the application.
+
+            IPAddress ipAddress = (IPAddress)this.cmbIP.SelectedItem;
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, int.Parse(this.txtServerPort.Text));
+
+            // Create a TCP/IP socket.
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind the socket to the local endpoint and 
+            // listen for incoming connections.
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+                this.txtStatus.Text += "Server init... OK \r\n";
+
+                // Start listening for connections.
+                while (true)
+                {
+                    this.txtStatus.Text += ("Waiting for a connection... \r\n");
+                    // Program is suspended while waiting for an incoming connection.
+                    Socket handler = listener.Accept();
+                    data = null;
+
+                    // An incoming connection needs to be processed.                    
+                    bytes = new byte[1024];
+                    int bytesRec = handler.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);                                        
+
+                    // Show the data on the console.
+                    this.txtStatus.Text += "Response Client: " + data + "\r\n";
+
+                    // Echo the data back to the client.
+                    byte[] msg = Encoding.ASCII.GetBytes("PONG!");
+
+                    handler.Send(msg);
+                    this.txtStatus.Text += "Server Send: PONG! \r\n";
+
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                    
+                    if (data == "PING!")
+                    {
+                        break;
+                    }
+                                        
+                }
+            }
+            catch (Exception e)
+            {
+                this.txtStatus.Text += "$ ERROR: \r\n";
+                this.txtStatus.Text += e.Message;
+            }        
+        }
+
+
+        void StartClient()
+        {
+            // Data buffer for incoming data.
+            byte[] bytes = new byte[1024];
+
+            // Connect to a remote device.
+            try
+            {
+                // Establish the remote endpoint for the socket.
+                // This example uses port 11000 on the local computer.                
+                IPAddress ipAddress = IPAddress.Parse(this.txtIPServer.Text);
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, int.Parse(this.txtPortConnect.Text));
+
+                // Create a TCP/IP  socket.
+                Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                // Connect the socket to the remote endpoint. Catch any errors.
+                try
+                {
+                    this.txtStatus.Text += "Try connect... \r\n";
+                    this.Refresh();
+                    sender.Connect(remoteEP);
+                    this.txtStatus.Text += "Server connected! \r\n";
+
+                    // Encode the data string into a byte array.
+                    byte[] msg = Encoding.ASCII.GetBytes("PING!");
+
+                    // Send the data through the socket.
+                    int bytesSent = sender.Send(msg);
+                    this.txtStatus.Text += "Client Send: PING! \r\n";
+
+                    // Receive the response from the remote device.
+                    int bytesRec = sender.Receive(bytes);
+                    this.txtStatus.Text += "Server Response:" + Encoding.ASCII.GetString(bytes, 0, bytesRec) + "\r\n";
+
+                    // Release the socket.
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    this.txtStatus.Text += "$ ERROR: \r\n";
+                    this.txtStatus.Text += ane.Message;
+                }
+                catch (SocketException se)
+                {
+                    this.txtStatus.Text += "$ ERROR: \r\n";
+                    this.txtStatus.Text += se.Message;
+                }
+                catch (Exception e)
+                {
+                    this.txtStatus.Text += "$ ERROR: \r\n";
+                    this.txtStatus.Text += e.Message;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+            // Get host name
+            String strHostName = Dns.GetHostName();
+
+            // Find host by name
+            IPHostEntry iphostentry = Dns.GetHostByName(strHostName);
+
+            this.txtStatus.Text = "";
+            this.txtStatus.Text += "Your IP's: \r\n";
+            // Enumerate IP addresses
+            foreach (IPAddress ipaddress in iphostentry.AddressList)
+            {
+                this.txtStatus.Text += " > " + ipaddress + "\r\n";
+                this.cmbIP.Items.Add(ipaddress);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //  Thread th = new Thread(this.StartServer);
+            //  th.Start();
+            this.StartServer();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //            Thread th = new Thread(this.StartClient);
+            //            th.Start();
+            this.StartClient();
+        }
+    }
+}
